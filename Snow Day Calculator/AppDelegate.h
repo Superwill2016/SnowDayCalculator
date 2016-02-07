56f6404230a4bae7bd878617fe484e45365f259b
Daniel Katz <Daniel@nat-wireless-guest-reg-153-50.bu.edu>
2016-02-07 18:37:45 -0500

    Initial Commit

---
+++ Snow Day Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Snow Day Calculator
+//
+//  Created by Daniel Katz on 2/7/16.
+//  Copyright © 2016 Stratton Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Snow Day Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Snow Day Calculator
+//
+//  Created by Daniel Katz on 2/7/16.
+//  Copyright © 2016 Stratton Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
